3199a7e2d556c79a5208c6276b49458c007d2730
Jannes <37171@ma-web.nl>
2024-09-05 15:41:41 +0200

    done but not tested
    fck future jannes

--- vage_gamejam/Assets/Anthony/Scripts/cameraController.cs
+++ vage_gamejam/Assets/Anthony/Scripts/cameraController.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class cameraController : MonoBehaviour
 {
-    [SerializeField] public GameObject cam1;
-    [SerializeField] public GameObject StartButton;
+    public GameObject cam1;
+    public GameObject StartButton;
+    public Scene NextScene;
     // Start is called before the first frame update
     private void Start()
     {
-        Debug.Log("asdgiuasdgyugdsi");
+        //Debug.Log("asdgiuasdgyugdsi");
     }
     public void SwitchCamera()
     {
         cam1.SetActive(false);
         StartButton.SetActive(false);
+        SceneManager.LoadScene(NextScene.ToString());
     }
 }
---
+++ vage_gamejam/Assets/DatingController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DatingController : MonoBehaviour
+{
+    public string[] text = new string[4];
+    //[SerializeField] GameObject currentDialogObject;
+    [SerializeField] TMP_Text currentDialog;
+    [SerializeField] GameObject beginbutton;
+    [SerializeField] GameObject secondbutton;
+    [SerializeField] GameObject goodButton;
+    [SerializeField] GameObject badButton;
+    [SerializeField] Sprite[] images;
+    [SerializeField] Image currentImg;
+    [SerializeField] Scene nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //currentDialog = currentDialogObject.GetComponent<TextMeshPro>();
+        HideAllButtons();
+        currentDialog.text = text[0];
+        currentImg.sprite = images[0];
+    }
+
+    void HideAllButtons()
+    {
+        beginbutton.SetActive(false);
+        secondbutton.SetActive(false);
+        badButton.SetActive(false);
+        goodButton.SetActive(false);
+    }
+
+    public void SecondScene()
+    {
+        currentImg.sprite = images[1];
+        currentDialog.text = text[1];
+        HideAllButtons();
+        secondbutton.SetActive(true);
+    }
+    public void ThirdScene()
+    {
+        currentImg.sprite = images[2];
+        currentDialog.text = text[2];
+        HideAllButtons();
+        badButton.SetActive(true);
+        goodButton.SetActive(true);
+    }
+    public void FourthScene()
+    {
+        SceneManager.LoadScene(nextScene.ToString());
+    }
+}
